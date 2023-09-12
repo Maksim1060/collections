951d99c9de7abfccd5af49d331f2c19d622a16e0
Maksim <maks.kartashov.93@inbox.ru>
2023-09-12 23:28:49 +0200

    collections1

--- src/main/java/pro/sky/skyprocollections/controller/EmployeeController.java
+++ src/main/java/pro/sky/skyprocollections/controller/EmployeeController.java
@@ -1,4 +1,31 @@
 package pro.sky.skyprocollections.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import pro.sky.skyprocollections.model.Employee;
+import pro.sky.skyprocollections.service.EmployeeService;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employee")
 public class EmployeeController {
-}
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+@GetMapping("/add")
+    public Employee add(@RequestParam String firstName, @RequestParam String lastName) {
+        return employeeService.add(firstName, lastName);
+    }
+@GetMapping
+    public List<Employee> getAll() {
+        return employeeService.getAll();
+    }
+    }
+
+
+
--- src/main/java/pro/sky/skyprocollections/service/EmployeeService.java
+++ src/main/java/pro/sky/skyprocollections/service/EmployeeService.java
@@ -1,21 +1,28 @@
 package pro.sky.skyprocollections.service;
 
 import org.springframework.stereotype.Service;
+import pro.sky.skyprocollections.controller.EmployeeController;
 import pro.sky.skyprocollections.model.Employee;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class EmployeeService {
-    private Employee[] employees = new Employee[10];
-
+    private final List<Employee> employees = new ArrayList<>();
     public Employee add(String firstName, String lastName) {
-        return null;
+        Employee employee = new Employee(firstName, lastName);
+        employees.add(employee);
+        return employee;
     }
-
     public Employee remove(String firstName, String lastName) {
         return null;
     }
-
     public Employee get(String firstName, String lastName) {
         return null;
     }
-}
+    public List<Employee> getAll() {
+        return employees;
+    }
+    }
+
