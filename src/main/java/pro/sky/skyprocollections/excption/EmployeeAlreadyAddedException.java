208c825ca902edba4297fcd99bcefcee6aabf490
Maksim <maks.kartashov.93@inbox.ru>
2023-09-12 22:41:08 +0200

    collections1

---
+++ src/main/java/pro/sky/skyprocollections/excption/EmployeeAlreadyAddedException.java
@@ -0,0 +1,7 @@
+package pro.sky.skyprocollections.excption;
+
+public class EmployeeAlreadyAddedException extends RuntimeException{
+    public EmployeeAlreadyAddedException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/pro/sky/skyprocollections/service/EmployeeService.java
@@ -0,0 +1,21 @@
+package pro.sky.skyprocollections.service;
+
+import org.springframework.stereotype.Service;
+import pro.sky.skyprocollections.model.Employee;
+
+@Service
+public class EmployeeService {
+    private Employee[] employees = new Employee[10];
+
+    public Employee add(String firstName, String lastName) {
+        return null;
+    }
+
+    public Employee remove(String firstName, String lastName) {
+        return null;
+    }
+
+    public Employee get(String firstName, String lastName) {
+        return null;
+    }
+}
